ed1070ac4986dd7830fc3ebc244836330cecd0b5
zyin-08 <zhyin2608@gmail.com>
2026-09-21 15:46:59 +0200

    zyin/Fix: Create Player intermediate class #4

---
+++ src/worldcup/Player.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace WorldCup;
+
+/**
+ * Class to define the player
+ */
+class Player extends Person {
+    private string $position = "";
+
+    /**
+     * Get the position
+     */
+    public function getPosition(): string {
+        return $this->position;
+    }
+
+    /**
+     * Set the position
+     */
+    public function setPosition(string $position): void {
+        $this->position = $position;
+    }
+
+    public function passBall(): void {
+        echo "passing the ball\n";
+    }
+}
